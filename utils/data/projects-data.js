f7b74af10dd1440aa1cffa4757e69b5aa112b4cb
Jyoti mishra <jyotimishra0896@gmail.com>
2024-10-09 22:36:20 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -4,6 +4,17 @@ import musicPlayer from '/public/image/musicPlayer.jpg';
 export const projectsData = [
     {
         id: 1,
+        name: 'Aplomb Biz App',
+        description: "It's a web and mobile application delivers a secure and seamless shopping experience, enabling users to log in, access their personalized Aplomb dashboard, and make purchases through an integrated shopping cart system. Built with Ionic, Capacitor, and React JS, it provides a smooth, cross-platform experience on both Android and iOS.Material UI for an intuitive design, and real-time API integrations for efficient backend communication, the app ensures optimal performance across devices.Deployment and testing were conducted through Android Studio to guarantee a flawless user experience.",
+        tools: ['Javascript', 'React.js', 'Material UI', 'React Hooks', 'Ionic framework', 'Android Studio', 'Capacitor'],
+        code: '',
+        role: 'Frontend Developer',
+        demo: '',
+        image: '',
+        link: 'https://play.google.com/store/apps/details?id=com.aplomb.aplombbiz',
+    },
+    {
+        id: 2,
         name: 'Document Scanner App',
         description: "Trained and gained expertise in MERN stack web application development.Collaborated with the product team to gather and understand project requirements.Developed an Ionic mobile application for scanning documents in high resolution and storing them over cloud blob storage.Designed app pages using the Material UI component library and integrated a Java-based library with an Android wrapper.Managed app functionality, ensuring seamless document scanning and cloud storage.",
         tools: ['React JS', 'Java', 'Android Studio', 'Ionic', 'Material UI'],
@@ -14,7 +25,7 @@ export const projectsData = [
         link: 'https://github.com/iamjyotimishra/ionic-camera-app',
     },
     {
-        id: 2,
+        id: 3,
         name: 'Music-Player UI App',
         description: 'The app features a dynamic song list, search functionality, a "Now Playing" section with playback controls, and a background that changes based on album art. The "Top Tracks" section highlights favorite-marked songs. With smooth animations and song data fetched from a REST API.',
         tools: ['Javascript', 'React.js', 'Material UI', 'React Hooks', 'Custom Hooks', 'Rest API'],
@@ -25,7 +36,7 @@ export const projectsData = [
         link: 'https://music-player-ui-two.vercel.app/',
     },
     {
-        id: 3,
+        id: 4,
         name: 'Search Image App',
         description: 'This is a responsive React-based Image Search application that allows users to search for images using a search query. The application dynamically fetches images and displays the search results with pagination, making it easy to navigate through multiple pages of images.',
         tools: ['Javascript', 'React.js', 'Material UI', 'React Hooks', 'Custom Hooks', 'Axios'],
